1753d8fed3d831c08fbe5c7cf23ab6510b862c73
James <85806019+James-Discord@users.noreply.github.com>
2021-07-24 01:38:14 +0100

    Update unlink.js

--- commands/Account/user/unlink.js
+++ commands/Account/user/unlink.js
@@ -1,7 +1,10 @@
 module.exports.run = (client, message, args) => {
     userData.delete(message.author.id)
-    message.channel.send('You have unlinked this account!')
-
+    message.channel.send({
+        embeds: [new MessageEmbed().setTitle("Account Unlink!").setColor("#36393F")
+        .addField(`Your account has now been unlinked from this discord account!`)
+        .setFooter(`Requested by ${message.author.tag}`).setTimestamp()]
+    });
 }
 
 
