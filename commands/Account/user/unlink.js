81479524ba4595ed2ff117d800cc6c1f78c24e4a
James <85806019+James-Discord@users.noreply.github.com>
2021-07-24 01:41:00 +0100

    Update unlink.js

--- commands/Account/user/unlink.js
+++ commands/Account/user/unlink.js
@@ -1,21 +1,18 @@
 module.exports.run = (client, message, args) => {
-    userData.delete(message.author.id)
+    userData.delete(message.author.id) //This is where the bot removes the Panel account from the Dicord User
     message.channel.send({
         embeds: [new MessageEmbed().setTitle("Account Unlink").setColor("#36393F")
         .addField(`Your account has now been unlinked from this discord account!`)
-        .setFooter(`Requested by ${message.author.tag}`).setTimestamp()]
+        .setFooter(`Requested by ${message.author.tag}`).setTimestamp()]//This is just a simple little embeded to tell them that there account is unlinked
     });
 }
 
 
-/**
- * This is completely optional...
- */
 
 module.exports.info = {
-    name: 'unlink',// default = file name (without the extention)
-    description: "A command to unlink your account",// default is "None"
-    requiredPermission: null,// default is null
-    aliases: [ul], // default is null
-    usage: 'unlink' // default is null
+    name: 'unlink',
+    description: "A command to unlink your account",
+    requiredPermission: null,
+    aliases: [ul], 
+    usage: 'unlink'
 }
